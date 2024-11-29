85bcb82bf401a659a9beec22b2a66098ba71afee
Jan Kopecny <kopecny.jan@gmail.com>
2024-11-29 19:41:22 +0100

    add checking history feature and some styles

--- scripts.js
+++ scripts.js
@@ -3,6 +3,33 @@
 const checkBtn = document.querySelector("#check-btn");
 const showResult = document.querySelector("#result");
 const input = document.querySelector("#text-input");
+const historyEl = document.querySelector("#history");
+
+function saveToHistory(textFromInput, isPalindrome) {
+  const history = JSON.parse(localStorage.getItem("history")) || [];
+  history.push({
+    input: textFromInput,
+    result: isPalindrome ? "is" : "is not",
+  });
+  localStorage.setItem("history", JSON.stringify(history));
+}
+
+function renderHistory() {
+  const history = JSON.parse(localStorage.getItem("history")) || [];
+  historyEl.innerHTML = "";
+  history.forEach(({ input, result }, index) => {
+    const historyItem = document.createElement("div");
+    historyItem.className = "history-item";
+    historyItem.innerHTML = `
+      <p><strong>${index + 1}. Input:</strong> ${input}</p>
+      <p><strong>Result:</strong> <span style="color: ${
+        result === "is" ? "green" : "red"
+      };">${result}</span> a palindrome</p>
+      <hr>
+    `;
+    historyEl.appendChild(historyItem);
+  });
+}
 
 checkBtn.addEventListener("click", () => {
   const textFromInput = input.value;
@@ -17,6 +44,14 @@ checkBtn.addEventListener("click", () => {
     showResult.innerHTML = `<h2>${textFromInput}</h2> <span class="badge ${
       isPalindrome ? "text-bg-success" : "text-bg-danger"
     }">${isPalindrome ? "is" : "is not"}</span> a palindrome`;
+    showResult.classList.add("has-content");
+    saveToHistory(textFromInput, isPalindrome);
+    renderHistory();
+
     input.value = "";
   }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderHistory();
 });
